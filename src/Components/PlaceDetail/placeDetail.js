c01fc5b67dfb5230de7d307aa86701497744f03e
unknown <meshkatuddinahammed@outlook.com>
2023-03-23 16:31:02 +0600

    Added font awesome icons from react-native-vector-icons

--- src/Components/PlaceDetail/placeDetail.js
+++ src/Components/PlaceDetail/placeDetail.js
@@ -1,20 +1,21 @@
-import {View,Modal,Image,Text,Button} from 'react-native';
+import {View,Modal,Image,Text,Button,TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const PlaceDetail = props => {
     return (
         <Modal>
-            <View>
+            <View style={{alignItems:'center'}}> 
                 <Image source={props.place.image} style={{width:'100%',height:300}}/>
                 <Text style={{textAlign:'center',fontSize:40}}>
                       {props.place.value}
                 </Text>
                 <View>
-                    <Button title='Delete' color="red" 
-                            onPress={()=>props.handleDeleteItem(props.place.key)}
-                    />
-                    <Button title='Close' color='blue'
-                            onPress={()=>props.handleHideModal()}
-                    />
+                    <TouchableOpacity onPress={()=>props.handleDeleteItem(props.place.key)}>
+                        <Icon name='trash' size={60} color='red'></Icon>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>props.handleHideModal()}>
+                        <Icon name='times-circle' size={60}></Icon>
+                    </TouchableOpacity>
                 </View>
             </View>
         </Modal>
